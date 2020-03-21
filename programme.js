c347eaea5ac6aa42962b3425ba17a4c06564f275
AndreBoileau <49464875+AndreBoileau@users.noreply.github.com>
2020-03-21 18:35:30 -0400

    Add files via upload

--- programme.js
+++ programme.js
@@ -1,6 +1,6 @@
 function installerProgramme() { 
   if (sessionStorage.getItem("donnee") == null) {
-  		messageERREUR("Un programme vide a été transmis par l'éditeur.\nPeut-être parce que l'éditeur p5Visuel et la page d'exécution ne sont pas sur le même domaine web.");}
+  		messageERREUR("Ou bien cette page d'exécution n'a pas été ouverte par l'éditeur p5Visuel, ou bien un programme vide a été transmis par l'éditeur (peut-être parce que l'éditeur p5Visuel et la page d'exécution ne sont pas sur le même domaine web).");}
   eval(sessionStorage.getItem("donnee"));
 }
 
